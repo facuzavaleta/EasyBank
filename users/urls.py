cb1d4fe4496293de6162e5d3518405da6af55b3a
facuzavaleta89 <facuzavaleta89@gmail.com>
2023-03-30 22:06:48 -0500

    avanzado en el front, falta acomodar elcarousel

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
-
+app_name = "users"
 urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     path("register/", views.register, name="register"),
